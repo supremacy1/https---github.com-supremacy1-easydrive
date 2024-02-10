dc8ba7acbf49249fe2fd1733f5b9e0d7f7035d92
supremacy1 <andrewohejiedeogbu@yahoo.com>
2024-02-10 19:20:38 +0100

    next update

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,5 +1,21 @@
 import { SignIn } from "@clerk/nextjs";
-
+ import Image from "next/image";
 export default function Page() {
-  return <SignIn />;
+  return (
+  <>
+  <div>
+  <Image
+      src="/open.jpg"
+      width={900}
+      height={1000}
+      alt="Picture of the author"
+      className="object-contain h-full w-full"
+  
+  />
+  <div className="absolute top-20 right-0">
+ <SignIn />
+ </div>
+ </div>
+  </>
+  );
 }
