82b0b65d1ef415b3463810066d1fa249502b36a5
supremacy1 <andrewohejiedeogbu@yahoo.com>
2024-02-13 09:57:44 +0100

    update

--- app/components/Home/Inputitem.js
+++ app/components/Home/Inputitem.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Image from "next/image";
 function Inputitem() {
   return (
-    <div className='bg-slate'>
+    <div className='bg-slate-200 p-3 rounded-lg mt-3
+    flex items-center gap-4'>
         <Image src='/next.svg'
         width={20}
         height={20}
         />
-        <input type='text' placeholder='pickup loacation'></input>
+        <input type='text' placeholder='pickup loacation'
+        className='bg-transparent w-full'
+        />
     </div>
   )
 }
